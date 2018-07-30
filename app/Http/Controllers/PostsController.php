09ab2650d4e40fa659622e5cae07b1944ed8dbd9
Richard Memory <richardfmemory@gmail.com>
2018-07-29 20:43:45 -0600

    Add an example of a query scope

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -9,8 +9,15 @@ use App\Post;
 class PostsController extends Controller
 {
 	public function index() {
-		$posts = Post::orderBy('created_at', 'desc')->get();
-		return view('posts.index', compact('posts'));
+		$posts = Post::orderBy('created_at', 'desc')
+		->filter(request(['month', 'year']))
+		->get();
+
+		$archives = Post::selectRaw('year(created_at) as year, monthname(created_at) as month, count(*) as published')
+			->groupBy('year', 'month')
+			->orderByRaw('min(created_at) desc') 
+			->get();
+		return view('posts.index', compact('posts', 'archives'));
 	}
 
 	public function create() {
--- app/Post.php
+++ app/Post.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 use App\Comment;
 use App\User;
@@ -23,5 +24,15 @@ class Post extends Model
 
 	public function user() {
 		return $this->belongsTo(User::class);
+	}
+
+	public function scopeFilter($query, $filters) {
+		if (array_key_exists('month', $filters) && $month = $filters['month']) {
+			$query->whereMonth('created_at', Carbon::parse($month)->month);
+		}
+
+		if (array_key_exists('year', $filters) && $year = $filters['year']) {
+			$query->whereYear('created_at', $year);
+		}
 	}
 }
